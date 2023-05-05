b74571f35398e35889e8f35f9f64cddb7567482b
Brian Lawney <brianlawney@hsph.harvard.edu>
2023-05-05 11:30:55 -0400

    Fixing location of outputs

--- docker/run.sh
+++ docker/run.sh
@@ -25,5 +25,5 @@ esac
 
 OUTPUT="${INPUT_FILE%.*}"
 /opt/software/wigToBigWig $INPUT_FILE $CHR_SZ $OUTPUT.bw
-
-echo "{\"bw_file\": \"$OUTPUT.bw\"}" > outputs.json
+BN=$(dirname $INPUT_FILE)
+echo "{\"bw_file\": \"$OUTPUT.bw\"}" > $BN/outputs.json
